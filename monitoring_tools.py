b1f72853b79ab8891376193b3aeeb30aa74ba7ce
Paul Gierz <pgierz@awi.de>
2018-11-19 09:11:08 +0100

    gets average walltime from a log file

--- monitoring_tools.py
+++ monitoring_tools.py
@@ -11,7 +11,9 @@ import matplotlib.pyplot as plt
 import subprocess
 import batch_systems
 import xarray as xr
+import datetime
 
+from tqdm import tnrange, tqdm_notebook
 
 batch_system_dict = {
                 "ollie": batch_systems.slurm
@@ -33,7 +35,34 @@ class simulation:
                 self.expid = expid
                 self.exp_home = path
                 self.setup = None
+                self.progress = None
 
+        def __repr__(self):
+                return "A {setup} simulation".format(setup=self.setup)
+
+        def _repr_markdown_(self):
+                part1 = """ Here is some information concerning your simulation, `{expid}`. It is made of up: 
+## {setup}
+
+### Atmosphere:
+{}
+
+### Land Surface:
+{}
+
+### Ocean:
+{}
+
+### Ice:
+{}
+
+### Solid Earth:
+{}
+""".format(expid=self.expid, 
+                                        setup=self.setup.upper(),
+                                        *[component._repr_markdown_() if hasattr(component, "_repr_markdown_") else component for component in self.components])
+                part3 = self.batch_system._repr_html_()
+                return part1 + part3
 
 class awicm_pism(simulation):
         def __init__(self, expid, path):
@@ -44,8 +73,9 @@ class awicm_pism(simulation):
                 self.ocean = fesom(expid, path+"/awicm/")
                 self.ice = pism(expid, path+"/pism_standalone/")
                 self.solid_earth = None
-                components = [self.atmosphere, self.land_surface, self.ocean, self.ice, self.solid_earth]
-                self.components = [component for component in components if component is not None]
+                self.components = [self.atmosphere, self.land_surface, self.ocean, self.ice, self.solid_earth]
+                self.used_components = [component for component in self.components if component is not None]
+                self.awicm_log = logfile(path+"/scripts/"+expid+"_awicm.log")
 
 class awicm(simulation):
         def __init__(self, expid, path):
@@ -56,8 +86,8 @@ class awicm(simulation):
                 self.ocean = fesom(expid, path)
                 self.ice = None
                 self.solid_earth = None
-                components = [self.atmosphere, self.land_surface, self.ocean, self.ice, self.solid_earth]
-                self.components = [component for component in components if component is not None]
+                self.components = [self.atmosphere, self.land_surface, self.ocean, self.ice, self.solid_earth]
+                self.used_components = [component for component in self.components if component is not None]
 
 
 class component:
@@ -73,6 +103,8 @@ class component:
                 self.mondir = mondir or path+"/mon/"+self.__name__
                 self.outdatadir = outdatadir or path+"/outdata/"+self.__name__
                 self.restartdir = restartdir or path+"/restart/"+self.__name__
+
+                self._data_loaded = False
 
         def _list_dir(self, _type):
                 # TODO: This needs to throw away symlinks, we really only want **files**
@@ -114,8 +146,16 @@ class component:
                 self._data_loaded = True
 
         def __str__(self):
-                return "A {model} run".format(model=self.__name__.upper())
+                a = "An" if self.__name__[0] in "aeiou" else "A"
+                return "{a} {model} run".format(a=a, model=self.__name__.upper())
 
+        def _repr_markdown_(self):
+                a = "An" if self.__name__[0] in "aeiou" else "A"
+                return """{a} `{model}` run
+
+Data loaded: `{data_loaded}`        
+
+""".format(a=a, model=self.__name__.upper(), data_loaded=self._data_loaded)
 
 def non_time_coords(ds):
         return [v for v in ds.data_vars if 'time' not in ds[v].dims]
@@ -163,13 +203,38 @@ class villma(component):
                 super().__init__(*args)
 
 class logfile():
-        def __init__(self):
-                pass
+        def __init__(self, log):
+                log_dataframe = pd.read_table(log,
+                                sep=r" :  | -" ,
+                                skiprows=1,
+                                infer_datetime_format=True,
+                                names=["Date", "Message", "State"],
+                                engine='python', index_col=0)
+                middle_column = log_dataframe["Message"].apply(lambda x: pd.Series(str(x).split()))
+                log_dataframe.drop("Message", axis=1, inplace=True)
+                middle_column.columns = ["Run Number", "Exp Date", "Job ID"]
+                log_dataframe = pd.concat([log_dataframe, middle_column], axis=1)
+                log_dataframe.set_index(pd.to_datetime(log_dataframe.index), inplace=True)
+                self.dataframe = log_dataframe[~log_dataframe.State.str.contains("post")]
 
         def get_mean_walltime(self):
-                # TODO: find a way to get these times in the methods
-                time_diff = time_at_start - time_at_done
-                return time_diff
+                starts = self.dataframe[self.dataframe.State == " start"]
+                ends = self.dataframe[self.dataframe.State == " done"]
+                # Drop the duplicates if we start more than one time (e.g. by hand)
+                starts.drop_duplicates(subset="Run Number", keep="last", inplace=True)
+                ends.drop_duplicates(subset="Run Number", keep="last", inplace=True)
+
+                shorter_list = min(len(starts), len(ends))
+
+                starts = starts[:shorter_list]
+                ends = ends[:shorter_list]
+                self._starts = starts
+                self._ends = ends
+                starts = starts.index.tolist()
+                ends = ends.index.tolist()
+                diffs = [ends[i] - starts[i] for i in range(len(ends))]
+                average_timedelta = sum(diffs, datetime.timedelta(0)) / len(diffs)
+                return average_timedelta
 
         def get_queuing_walltime(self):
                 # TODO: find a way to get these times in the methods
