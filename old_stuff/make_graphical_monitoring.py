54e9f3a222c42685dd28cd77ad21b0d6d2b9ba42
Paul Gierz <pgierz@awi.de>
2019-06-28 09:22:00 +0200

    Keeping an up to date copy of old stuff in the history before we trash it, maybe it was useful

--- old_stuff/make_graphical_monitoring.py
+++ old_stuff/make_graphical_monitoring.py
@@ -1,12 +1,15 @@
-import sys,os
-IPYNB_FILENAME = 'Monitoring.ipynb'
-CONFIG_FILENAME = '.config_ipynb'
+import sys, os
+
+IPYNB_FILENAME = "Monitoring.ipynb"
+CONFIG_FILENAME = ".config_ipynb"
+
 
 def main(argv):
-        with open(CONFIG_FILENAME,'w') as f:
-                f.write(' '.join(argv))
-        os.system('jupyter nbconvert --execute {:s} --to html'.format(IPYNB_FILENAME))
-        return None
+    with open(CONFIG_FILENAME, "w") as f:
+        f.write(" ".join(argv))
+    os.system("jupyter nbconvert --execute {:s} --to html".format(IPYNB_FILENAME))
+    return None
 
-if __name__ == '__main__':
-        main(sys.argv)
+
+if __name__ == "__main__":
+    main(sys.argv)
