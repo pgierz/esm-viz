111a43a3d2c02693dd59635367c63c70c51dddc1
Paul Gierz <pgierz@awi.de>
2019-07-25 21:50:27 +0100

    shebang

--- esm_viz/cli.py
+++ esm_viz/cli.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 """Console script for esm_viz."""
@@ -24,9 +25,15 @@ module_path = os.path.dirname(inspect.getfile(esm_viz))
 @click.group(invoke_without_command=True)
 @click.version_option()
 @click.pass_context
-@click.option("--expid", default="example", help="The YAML file found in ~/.config/monitoring")
-@click.option("--frequency", default=2, help="How often to run monitoring for this experiment (Default is every 2 hours)")
-@click.option('--quiet', default=False, is_flag=True)
+@click.option(
+    "--expid", default="example", help="The YAML file found in ~/.config/monitoring"
+)
+@click.option(
+    "--frequency",
+    default=2,
+    help="How often to run monitoring for this experiment (Default is every 2 hours)",
+)
+@click.option("--quiet", default=False, is_flag=True)
 def main(ctx, expid, frequency, quiet):
     if ctx.invoked_subcommand is None:
         click.echo("Scheduling...")
@@ -36,9 +43,16 @@ def main(ctx, expid, frequency, quiet):
         click.echo("Combining...")
         ctx.invoke(combine, expid=expid, quiet=quiet)
 
+
 @main.command()
-@click.option("--expid", default="example", help="The YAML file found in ~/.config/monitoring")
-@click.option("--frequency", default=2, help="How often to run monitoring for this experiment (Default is every 2 hours)")
+@click.option(
+    "--expid", default="example", help="The YAML file found in ~/.config/monitoring"
+)
+@click.option(
+    "--frequency",
+    default=2,
+    help="How often to run monitoring for this experiment (Default is every 2 hours)",
+)
 def schedule(expid, frequency):
     """
     Schedule a job for automatic monitoring
@@ -52,18 +66,30 @@ def schedule(expid, frequency):
     """
     cron = CronTab(user=True)
     job = cron.new(
-            command='source activate pyviz; /scratch/work/pgierz/anaconda3/bin/esm_viz deploy --expid '+expid+' && /scratch/work/pgierz/anaconda3/bin/esm_viz combine --expid '+expid,
-            comment='Monitoring for '+expid)
-    job.env['PATH'] = "/scratch/work/pgierz/anaconda3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+        command="source activate pyviz; /scratch/work/pgierz/anaconda3/bin/esm_viz deploy --expid "
+        + expid
+        + " && /scratch/work/pgierz/anaconda3/bin/esm_viz combine --expid "
+        + expid,
+        comment="Monitoring for " + expid,
+    )
+    job.env[
+        "PATH"
+    ] = "/scratch/work/pgierz/anaconda3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
     job.every(frequency).hours()
     if job.is_valid():
         job.enable()
         cron.write()
-        click.echo("Successfully scheduled automatic monitoring of %s every %s hours" % (expid, frequency))
+        click.echo(
+            "Successfully scheduled automatic monitoring of %s every %s hours"
+            % (expid, frequency)
+        )
+
 
 @main.command()
-@click.option('--quiet', default=False, is_flag=True)
-@click.option('--expid', default="example", help="The YAML file found in ~/.config/monitoring")
+@click.option("--quiet", default=False, is_flag=True)
+@click.option(
+    "--expid", default="example", help="The YAML file found in ~/.config/monitoring"
+)
 def deploy(expid, quiet):
     """
     Deploys a script to a computation host (supercompute) and runs it.
@@ -84,23 +110,25 @@ def deploy(expid, quiet):
         config = read_simulation_config(expid)
     else:
         config = read_simulation_config(
-            os.environ.get("HOME")+"/.config/monitoring/"+expid+".yaml"
-            )
-    monitor = Simulation_Monitor(
-        config.get('user'),
-        config.get('host'),
-        config.get('basedir'),
-        config.get("coupling", False),
-        config.get('storagedir'),
-        config.get('required_modules', ['anaconda3', 'cdo'])
+            os.environ.get("HOME") + "/.config/monitoring/" + expid + ".yaml"
         )
+    monitor = Simulation_Monitor(
+        config.get("user"),
+        config.get("host"),
+        config.get("basedir"),
+        config.get("coupling", False),
+        config.get("storagedir"),
+        config.get("required_modules", ["anaconda3", "cdo"]),
+    )
 
-    analysis_script_path = module_path+"/analysis"
+    analysis_script_path = module_path + "/analysis"
 
     for component in MODEL_COMPONENTS.get(config["model"]):
         if component in config:
-            for monitoring_part in ['Global Timeseries', 'Global Climatology']:
-                monitoring_part_script_string = monitoring_part.replace(' ', '_').lower()
+            for monitoring_part in ["Global Timeseries", "Global Climatology"]:
+                monitoring_part_script_string = monitoring_part.replace(
+                    " ", "_"
+                ).lower()
                 if monitoring_part in config[component]:
                     # In YAML, the variable vairable_container comes back as a
                     # dictionary, so we need to unpack a bit:
@@ -109,96 +137,153 @@ def deploy(expid, quiet):
                     for variable in config[component][monitoring_part]:
                         container = config[component][monitoring_part][variable]
                         logging.debug(container)
-                        file_pattern = container['file pattern']
+                        file_pattern = container["file pattern"]
                         args = [variable, file_pattern]
-                        if 'analysis script' in container:
-                            specialized_script = container['analysis script'][0]
-                            if len(container['analysis script']) > 1:
-                                args = args + container['analysis script'][1:]
+                        if "analysis script" in container:
+                            specialized_script = container["analysis script"][0]
+                            if len(container["analysis script"]) > 1:
+                                args = args + container["analysis script"][1:]
                             if os.path.isfile(specialized_script):
                                 script_to_run = specialized_script
                         else:
-                            script_to_run = analysis_script_path+'/'+component+'/monitoring_'+component+'_'+monitoring_part_script_string+'.sh'
+                            script_to_run = (
+                                analysis_script_path
+                                + "/"
+                                + component
+                                + "/monitoring_"
+                                + component
+                                + "_"
+                                + monitoring_part_script_string
+                                + ".sh"
+                            )
                         if not os.path.isfile(script_to_run):
-                            logging.error('The analysis script you want to copy to the computer server does not exist!')
-                            logging.error('It was %s', script_to_run)
+                            logging.error(
+                                "The analysis script you want to copy to the computer server does not exist!"
+                            )
+                            logging.error("It was %s", script_to_run)
                             sys.exit(1)
                         monitor.copy_analysis_script_for_component(
-                            component,
-                            script_to_run
-                            )
+                            component, script_to_run
+                        )
                         monitor.run_analysis_script_for_component(
-                            component,
-                            script_to_run,
-                            args
-                            )
+                            component, script_to_run, args
+                        )
                         monitor.copy_results_from_analysis_script(
-                            component,
-                            variable,
-                            monitoring_part,
-                            )
+                            component, variable, monitoring_part
+                        )
                         # Sleep for 1 second to avoid timeout errors:
                         time.sleep(1)
-            for monitoring_part in ['Special Timeseries']:
+            for monitoring_part in ["Special Timeseries"]:
                 if monitoring_part in config[component]:
                     for special_timeseries in config[component][monitoring_part]:
                         # Did the user give a full path?
-                        if 'script' in special_timeseries:
-                            if os.path.isfile(special_timeseries.get('script')):
-                                special_timeseries_script = special_timeseries.get('script')
-                        else: # we assume its in the analysis/component directory
-                            special_timeseries_script = analysis_script_path+"/"+component+"/monitoring_"+component+"_"+monitoring_part.replace(" ", "_").lower()+".py"
+                        if "script" in special_timeseries:
+                            if os.path.isfile(special_timeseries.get("script")):
+                                special_timeseries_script = special_timeseries.get(
+                                    "script"
+                                )
+                        else:  # we assume its in the analysis/component directory
+                            special_timeseries_script = (
+                                analysis_script_path
+                                + "/"
+                                + component
+                                + "/monitoring_"
+                                + component
+                                + "_"
+                                + monitoring_part.replace(" ", "_").lower()
+                                + ".py"
+                            )
                         # Did we get args?
-                        if 'args' in special_timeseries:
-                            special_timeseries_args = special_timeseries.get('args')
+                        if "args" in special_timeseries:
+                            special_timeseries_args = special_timeseries.get("args")
                         else:
                             special_timeseries_args = []
-                        monitor.copy_analysis_script_for_component(component, special_timeseries_script)
-                        monitor.run_analysis_script_for_component(component, special_timeseries_script, special_timeseries_args)
+                        monitor.copy_analysis_script_for_component(
+                            component, special_timeseries_script
+                        )
+                        monitor.run_analysis_script_for_component(
+                            component,
+                            special_timeseries_script,
+                            special_timeseries_args,
+                        )
 
 
 @main.command()
-@click.option('--quiet', default=False, is_flag=True)
-@click.option('--expid', default="example", help="The YAML file found in ~/.config/monitoring")
+@click.option("--quiet", default=False, is_flag=True)
+@click.option(
+    "--expid", default="example", help="The YAML file found in ~/.config/monitoring"
+)
 def combine(expid, quiet):
     if quiet:
         logging.basicConfig(level=logging.ERROR)
     else:
         logging.basicConfig(level=logging.INFO)
-    config = read_simulation_config(os.environ.get("HOME")+"/.config/monitoring/"+expid+".yaml")
+    config = read_simulation_config(
+        os.environ.get("HOME") + "/.config/monitoring/" + expid + ".yaml"
+    )
     # Remove stuff from the config that we probably won't need:
-    for bad_chapter in ['user', 'host', 'basedir', 'coupling']:
+    for bad_chapter in ["user", "host", "basedir", "coupling"]:
         if bad_chapter in config:
             del config[bad_chapter]
-    viz_path = module_path+"/visualization/"
-    notebooks_to_merge = [viz_path+"read_config.ipynb"]
-    for monitoring_element in config['general']:
-        if os.path.isfile(viz_path+"general_"+monitoring_element.lower().replace(" ", "_")+".ipynb"):
-            notebooks_to_merge.append(viz_path+'general_'+monitoring_element.lower().replace(" ", "_")+".ipynb")
+    viz_path = module_path + "/visualization/"
+    notebooks_to_merge = [viz_path + "read_config.ipynb"]
+    for monitoring_element in config["general"]:
+        if os.path.isfile(
+            viz_path
+            + "general_"
+            + monitoring_element.lower().replace(" ", "_")
+            + ".ipynb"
+        ):
+            notebooks_to_merge.append(
+                viz_path
+                + "general_"
+                + monitoring_element.lower().replace(" ", "_")
+                + ".ipynb"
+            )
     for component in MODEL_COMPONENTS.get(config["model"]):
         if component in config:
-            for monitoring_part in ['Global Timeseries', 'Global Climatology']:
+            for monitoring_part in ["Global Timeseries", "Global Climatology"]:
                 if monitoring_part in config[component]:
-                    notebooks_to_merge.append(viz_path+component+'_'+monitoring_part.replace(' ', '_').lower()+'.ipynb')
-    if 'custom_notebooks' in config:
-        for notebook in config['custom_notebooks']:
+                    notebooks_to_merge.append(
+                        viz_path
+                        + component
+                        + "_"
+                        + monitoring_part.replace(" ", "_").lower()
+                        + ".ipynb"
+                    )
+    if "custom_notebooks" in config:
+        for notebook in config["custom_notebooks"]:
             notebooks_to_merge.append(notebook)
     # Add chapters at the end:
-    appendix_chapters = ['last_update.ipynb']
+    appendix_chapters = ["last_update.ipynb"]
     for appendix_chapter in appendix_chapters:
-        notebooks_to_merge.append(viz_path+"/"+appendix_chapter)
+        notebooks_to_merge.append(viz_path + "/" + appendix_chapter)
     print(notebooks_to_merge)
-    with open(expid+".ipynb", "w") as notebook_merged:
+    with open(expid + ".ipynb", "w") as notebook_merged:
         notebook_merged.write(merge_notebooks(notebooks_to_merge))
     with open(".config_ipynb", "w") as config_file:
-        config_file.write(' '.join(['test_args.py', os.environ.get("HOME")+"/.config/monitoring/"+expid+".yaml"]))
+        config_file.write(
+            " ".join(
+                [
+                    "test_args.py",
+                    os.environ.get("HOME") + "/.config/monitoring/" + expid + ".yaml",
+                ]
+            )
+        )
     if not quiet:
         click.echo("Combined notebook; executing and converting to HTML")
-    os.system('/scratch/work/pgierz/anaconda3/envs/pyviz/bin/jupyter nbconvert --execute {:s} --to html'.format(expid+".ipynb"))
-    os.rename(expid+".html", os.environ.get("HOME")+"/public_html/"+expid+".html")
+    os.system(
+        "/scratch/work/pgierz/anaconda3/envs/pyviz/bin/jupyter nbconvert --execute {:s} --to html".format(
+            expid + ".ipynb"
+        )
+    )
+    os.rename(
+        expid + ".html", os.environ.get("HOME") + "/public_html/" + expid + ".html"
+    )
     if not quiet:
         click.echo("Cleaning up...")
     os.remove(".config_ipynb")
+
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
