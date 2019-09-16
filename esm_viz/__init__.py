00703e1ec1b19b1e56c8df5186276c23e32e1e60
Paul Gierz <pgierz@awi.de>
2019-09-16 16:50:43 +0200

    Bump version: 0.9.4 → 0.9.5

--- esm_viz/__init__.py
+++ esm_viz/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Paul Gierz"""
 __email__ = "pgierz@awi.de"
-__version__ = "0.9.4"
+__version__ = "0.9.5"
--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/pgierz/esm_viz",
-    version="0.9.4",
+    version="0.9.5",
     zip_safe=False,
 )
