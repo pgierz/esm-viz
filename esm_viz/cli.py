088aa782b088659c94e6236b249da09b35269ecc
Paul Gierz <pgierz@awi.de>
2019-09-27 08:59:51 +0200

    think i found it again

--- esm_viz/cli.py
+++ esm_viz/cli.py
@@ -245,7 +245,7 @@ def combine(expid, quiet):
         logging.info("Setting up general monitoring")
         general_mon = general.GeneralPanel.from_config(config)
         pane = general_mon.render_pane(config)
-        tab_list.append(pane)
+        tab_list.append(("General", *pane))
     for component in MODEL_COMPONENTS.get(config["model"]):
         if component in config:
             logging.info("Setting up monitoring for %s", component)
