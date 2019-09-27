56af2e7c8b861e77f556fa20200c2f7c1efe51cf
Paul Gierz <pgierz@awi.de>
2019-09-27 08:33:05 +0200

    fixup general tab, maybe

--- esm_viz/cli.py
+++ esm_viz/cli.py
@@ -244,7 +244,8 @@ def combine(expid, quiet):
     if "general" in config:
         logging.info("Setting up general monitoring")
         general_mon = general.GeneralPanel.from_config(config)
-        tab_list.append(("General", general_mon.render_pane(config)))
+        pane = general_mon.render_pane(config)
+        tab_list.append(("General", *pane))
     for component in MODEL_COMPONENTS.get(config["model"]):
         if component in config:
             logging.info("Setting up monitoring for %s", component)
