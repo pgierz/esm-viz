820796d1585ecce9f37b819fc12723b2562fe518
Paul Gierz <pgierz@awi.de>
2019-09-27 09:10:29 +0200

    OK...?

--- esm_viz/visualization/general.py
+++ esm_viz/visualization/general.py
@@ -137,7 +137,7 @@ class GeneralPanel(Simulation_Monitor):
         if "newest log" in config["general"]:
             pass  # NotYetImplemented
 
-        return pn.Tabs(General_Tabs)
+        return pn.Tabs(*General_Tabs)
 
 
 class General(Simulation_Monitor):
