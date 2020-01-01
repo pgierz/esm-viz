aae7dfa917a400928102542398ab611e31b6ead1
Paul Gierz <pgierz@awi.de>
2020-01-01 12:19:55 +0100

    some of login

--- esm_viz/server/__init__.py
+++ esm_viz/server/__init__.py
@@ -1,26 +1,55 @@
-# Hello, Flask!
-from flask import Flask, render_template, request
+# Flask
+from flask import Flask, render_template, request, session, redirect, url_for, escape
+from simplepam import authenticate
 
 
 # Queue stuff:
 
 
 app = Flask(__name__)
+app.secret_key = "!098abctheowlisfluffyxyz123+"
 
-# Index page, no args
-@app.route('/')
+
+@app.route("/")
 def index():
-    name = request.args.get("name")
-    if name == None:
-        name = "Paul"
-        return render_template("index.html", name=name)
+    if "username" in session:
+        print(session)
+        return "Logged in as %s" % escape(session["username"])
+    return "You are not logged in"
 
 
-@app.route('/queue')
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if authenticate(str(username), str(password)):
+            session["username"] = request.form["username"]
+            return redirect(url_for("index"))
+        else:
+            return "Invalid username/password"
+    return """
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=password name=password>
+            <p><input type=submit value=Login>
+        </form>
+    """
+
+
+@app.route("/logout")
+def logout():
+    # remove the username from the session if it's there
+    session.pop("username", None)
+    return redirect(url_for("index"))
+
+
+# TODO: This should just be part of the dashboard
+@app.route("/queue")
 def queue():
     return "This is the queue!!"
 
-# With debug=True, Flask server will auto-reload 
+# With debug=True, Flask server will auto-reload
 # when there are code changes
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=8888, debug=True)
